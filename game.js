4f1f3b8f9a0c3c6ff3fe8b6b7bc59bd07ef2beb2
lbayley512 <lbayley512@gmail.com>
2023-06-24 15:01:22 -0700

    adding boundary

--- game.js
+++ game.js
@@ -1,3 +1,28 @@
-const canvas = document.querySelector('canvas')
-const ctx = canvas.getContext("2d");
-console.log(canvas)
+canvas = document.getElementById('game-canvas');
+context = canvas.getContext("2d");
+
+canvas.height = innerWidth;
+canvas.height = innerHeight;
+
+class boundary {
+    constructor({position}) {
+        this.posistion = position;
+        this.width = 40
+        this.height = 40
+    }
+
+    draw() {
+        ctx.fillstyle = "blue"
+        ctx.fillReact(this.posistion.x, this.posistion.y, this.width, this.height)
+
+    }
+}
+
+const boundaries = [
+    new boundary({
+        position: {
+            x: 0,
+            y: 0
+        }
+    })
+]
